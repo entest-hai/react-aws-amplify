6479e9a48e3378322887d8a058a611dde2c8dfc0
Tran Minh Hai <hai@bio-rithm.com>
2022-01-16 11:58:36 +0700

    update lambda api endpoint

--- src/config/apiendpoint.js
+++ src/config/apiendpoint.js
@@ -1,5 +1,6 @@
 import awsmobile from '../aws-exports';
-const fhr_lambda_api = "https://1h23llaoi0.execute-api.ap-southeast-1.amazonaws.com/dev/lambda-fhr/lambda-1?filename=";
+// const fhr_lambda_api = "https://1h23llaoi0.execute-api.ap-southeast-1.amazonaws.com/dev/lambda-fhr/lambda-1?filename=";
+const fhr_lambda_api = "https://bln9cf30wj.execute-api.ap-southeast-1.amazonaws.com/default/femomfhr?filename";
 let fhr_api_end_point = fhr_lambda_api + "s3://" + awsmobile.aws_user_files_s3_bucket + "/public/"; 
 
 export {fhr_api_end_point};
